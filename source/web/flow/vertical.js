2f03430b77df7cc521911073b409524be79944f2
dikers <dikers@gmail.com>
2020-05-27 19:10:23 +0800

    add image

--- source/web/flow/vertical.js
+++ source/web/flow/vertical.js
@@ -95,7 +95,7 @@ function create_vertical_table_template(table_block_id){
 
     vue.currentTableBlock['row_poz_list'] = row_poz_list
     vue.currentTableBlock['col_poz_list'] = col_poz_list
-    split_vertical_td_by_col_row(row_poz_list, col_poz_list)
+    split_vertical_td_by_col_row(thItems, row_poz_list, col_poz_list)
     redraw_canvas()
 
 
@@ -118,7 +118,8 @@ function  find_first_block_in_line(thItems){
         }
     }
 
-    new_item_poz_list.push(new_item_poz_list[new_item_poz_list.length-1] + vue.currentTableBlock['row_max_height'])
+    new_item_poz_list.push(new_item_poz_list[new_item_poz_list.length-1] +
+                    parseInt(vue.currentTableBlock['row_max_height']))
     console.log("new_th_item_list: ", JSON.stringify(new_item_poz_list ))
 
 
@@ -138,8 +139,40 @@ function  find_first_block_in_line(thItems){
 /**
 划分行内元素
 */
-function split_vertical_td_by_col_row(row_poz_list, col_poz_list){
+function split_vertical_td_by_col_row(thItems, row_poz_list, col_poz_list){
+
+
+    var blockItemList = vue.blockItemList
+
+    var table_row_list = [] // {'key': key , 'value': value}
+
+
+    for (var row of row_poz_list){
+        var box = {'left': col_poz_list[0]['left'], 'right': col_poz_list[0]['right'] ,
+                     'top': row['top'], 'bottom': row['bottom'] }
+
+        var find_row_block_list = find_block_list_in_box(box)
+    }
 
 
 
+
+}
+
+function find_block_list_in_box(box){
+   console.log("^^^^^^^^box  ", JSON.stringify(box))
+   var block_list = []
+   for(var blockItem of vue.blockItemList){
+        if (blockItem['raw_block_type'] =='LINE'){
+            continue
+        }
+        if ( blockItem['x'] > box['left'] && blockItem['x']<= box['right']
+           &&  blockItem['y']> box['top'] && blockItem['y'] <= box['bottom']){
+            block_list.push(blockItem)
+            console.log("***********   ", blockItem['text'])
+
+        }
+   }
+
+    return block_list
 }
